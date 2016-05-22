09a9127d267acfe87e6d688bd083ee4c0fffb5ba
sebastijan94 <sebastijan94@yahoo.com>
2016-05-22 20:32:40 +0200

    dragndrop

--- HCI_Project/HCI_Project/MainWindow.xaml.cs
+++ HCI_Project/HCI_Project/MainWindow.xaml.cs
@@ -28,6 +28,9 @@ namespace HCI_Project
         public RepoLokali repoLokali { get; set; }
         public RepoEtikete repoEtikete { get; set; }
 
+        Image drag_image = null;
+        Point _DragStart = new Point();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -50,11 +53,11 @@ namespace HCI_Project
 
 
             Uri myUri = new Uri("../../map.jpg", UriKind.RelativeOrAbsolute);
-            JpegBitmapDecoder decoder2 = new JpegBitmapDecoder(myUri, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-            BitmapSource bitmapSource2 = decoder2.Frames[0];
+            JpegBitmapDecoder decoder = new JpegBitmapDecoder(myUri, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+            BitmapSource bitmapSource = decoder.Frames[0];
 
             // Draw the Image
-            myImage.Source = bitmapSource2;
+            myImage.Source = bitmapSource;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -206,5 +209,125 @@ namespace HCI_Project
             return null;
         }
 
+        private void treeView1_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _DragStart = e.GetPosition(null);
+        }
+
+        private void treeView1_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            Point mousePos = e.GetPosition(null);
+            Vector diff = _DragStart - mousePos;
+
+            if (e.LeftButton == MouseButtonState.Pressed &&
+             Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance &&
+             Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance)
+            {
+                TreeView treeView = sender as TreeView;
+                TreeViewItem treeViewItem = FindAncestor<TreeViewItem>((DependencyObject)e.OriginalSource);
+                if (treeViewItem != null)
+                {
+                    //Find its parent
+                    ItemsControl parent = FindParent<ItemsControl>(treeViewItem);
+                    //Get the bound object.
+                    object item = parent.ItemContainerGenerator.ItemFromContainer(treeViewItem);
+                    if (item != null)
+                    {
+                        if (item.GetType() == typeof(Lokal)) {
+                            DataObject dragData = new DataObject("myFormat", (Lokal)item);
+                            DragDrop.DoDragDrop(treeViewItem, dragData, DragDropEffects.Move);
+                        }
+                    }
+                }
+            }
+        }
+
+        //Get the parent of an item.
+        private static T FindParent<T>(FrameworkElement current)
+          where T : FrameworkElement
+        {
+            do
+            {
+                current = VisualTreeHelper.GetParent(current) as FrameworkElement;
+                if (current is T)
+                {
+                    return (T)current;
+                }
+            }
+            while (current != null);
+            return null;
+        }
+
+        // Helper to search up the VisualTree
+        private static T FindAncestor<T>(DependencyObject current)
+          where T : DependencyObject
+        {
+            do
+            {
+                if (current is T)
+                {
+                    return (T)current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            }
+            while (current != null);
+            return null;
+        }
+
+        private void Canvas_StartDrag(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is Image)
+            {
+                mapa.CaptureMouse();
+
+
+                drag_image = e.OriginalSource as Image;
+                DragDrop.DoDragDrop(this, drag_image, DragDropEffects.Move);
+
+            }
+        }
+
+        private void Canvas_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("myFormat"))
+            {
+                Console.WriteLine("AAAAAAAA");
+                Lokal lokal = e.Data.GetData("myFormat") as Lokal;
+
+                Uri myUri = new Uri(lokal.Slika, UriKind.RelativeOrAbsolute);
+                JpegBitmapDecoder decoder = new JpegBitmapDecoder(myUri, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+                BitmapSource bitmapSource = decoder.Frames[0];
+
+                drag_image = new Image();
+                drag_image.Source = bitmapSource;
+                drag_image.Height = 32;
+                drag_image.Width = 32;
+                // Initialize the drag & drop operation
+                mapa.Children.Add(drag_image);
+                Point point = new Point();
+                point = e.GetPosition(mapa);
+                Canvas.SetLeft(drag_image, point.X);
+                Canvas.SetTop(drag_image, point.Y);
+
+                drag_image = null;
+            }
+            else {
+                Point point = new Point();
+                point = e.GetPosition(mapa);
+                Canvas.SetLeft(drag_image, point.X);
+                Canvas.SetTop(drag_image, point.Y);
+
+                drag_image = null;
+            }
+            
+        }
+
+        private void Canvas_DragEnter(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent("myFormat") || sender == e.Source)
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
     }
 }
